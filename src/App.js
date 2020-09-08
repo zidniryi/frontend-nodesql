c8a4105e8b4ae9816e2d9f2273cad6a20184155a
zidniryi <zidnirydwan@gmail.com>
2020-09-08 22:33:51 +0700

    feat(src): create entry agen ui

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Fragment, useEffect } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './home/Home'
+import EntryAgen from './entryAgen/EntryAgen'
 
 const App = () => {
   return (
     <Router>
       <Fragment>
         <Route exact path={'/'} component={Home} />
+        <Route exact path={'/entry-agen'} component={EntryAgen} />
       </Fragment>
     </Router>
   )
--- src/entryAgen/EntryAgen.js
+++ src/entryAgen/EntryAgen.js
@@ -1,37 +1,18 @@
 import React, { Fragment } from 'react'
 import NavBar from '../components/NavBar'
 
-const Home = () => {
+const EntryAgen = () => {
   return (
     <Fragment>
       <NavBar />
       <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 class="display-4">INPUT DATA AGEN</h1>
+        <h1 class="display-4">INPUT ENTRY AGEN</h1>
       </div>
 
       <div class="container">
         <form>
           <div class="form-group">
-            <label for="exampleInputEmail1">No Lisensi</label>
-            <input
-              type="number"
-              class="form-control"
-              aria-describedby="emailHelp"
-              placeholder="Nomor Lisensi"
-              name="licenseNumber"
-            />
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Nama Agen</label>
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Nama Agen"
-              name="agenName"
-            />
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Tipe Agen</label>
+            <label for="exampleInputPassword1">Pilih Nama Agen</label>
             <select
               class="form-control"
               id="exampleFormControlSelect1"
@@ -39,7 +20,7 @@ const Home = () => {
               style={{ height: 40 }}
               // onChange={(e) => onChange(e)}
             >
-              <option value="">Level</option>
+              <option value="">Nama Agen</option>
               <option value="RM">RM</option>
               <option value="RM">SM</option>
               <option value="UM">UM</option>
@@ -48,12 +29,39 @@ const Home = () => {
           </div>
 
           <div class="form-group">
-            <label for="exampleInputPassword1">Wilayah</label>
-            <input
-              type="text"
+            <label for="exampleInputPassword1">Nama Atasan</label>
+            <select
               class="form-control"
-              placeholder="Wilayah"
-              name="area"
+              id="exampleFormControlSelect1"
+              name="Level"
+              style={{ height: 40 }}
+              // onChange={(e) => onChange(e)}
+            >
+              <option value="">Nama Atasan</option>
+              <option value="RM">RM</option>
+              <option value="RM">SM</option>
+              <option value="UM">UM</option>
+              <option value="FU">FU</option>
+            </select>
+          </div>
+
+          <div class="form-group">
+            <label for="exampleInputPassword1">Mulai Berlaku</label>
+            <input
+              type="date"
+              class="form-control"
+              placeholder="Tanggal"
+              name="startDate"
+            />
+          </div>
+
+          <div class="form-group">
+            <label for="exampleInputPassword1">Akhir Berlaku</label>
+            <input
+              type="date"
+              class="form-control"
+              placeholder="Tanggal"
+              name="endDate"
             />
           </div>
           <div class="form-check">
@@ -75,4 +83,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default EntryAgen
